d24aa0d044d221e0bb0771d68abf1bd679407ebe
Priyansh <priyanshpatel234@gmail.com>
2024-07-04 13:50:16 +0530

    create user model

---
+++ server/models/user-model.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose, { Types } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = new mongoose.model("User", userSchema);
+
+export default User;
