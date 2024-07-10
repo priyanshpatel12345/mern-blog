103c9462ead852cc58b1bd1b857ed779e3b43666
Priyansh <priyanshpatel234@gmail.com>
2024-07-11 01:57:46 +0530

    complete create a post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,9 +21,10 @@ import {
   signOutSuccess,
 } from "../app/user/userSlice";
 import { Modal } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileURL, setImageFileURL] = useState(null);
   const filePickerRef = useRef();
@@ -244,9 +245,26 @@ export default function DashProfile() {
           onChange={handleInput}
         />
 
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          disabled={loading || imageFileUpload}
+        >
+          {loading ? "Loading..." : "Update"}
         </Button>
+
+        <Link to={"/create-post"}>
+          {currentUser.isAdmin && (
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create a post
+            </Button>
+          )}
+        </Link>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,57 @@
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function CreatePost() {
+  return (
+    <div className="sm:p-20 p-7 max-3xl mx-auto min-h-screen">
+      <h1 className="text-center font-semibold text-3xl my-7 text-black ">
+        Create a Post
+      </h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Title "
+            required
+            id="title"
+            className="flex-1"
+          />
+          <Select>
+            <option value="uncategorized">Select a category</option>
+            <option value="javascript">Javascript</option>
+            <option value="reactjs">React.js</option>
+            <option value="nextjs">Next.js</option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4 border-dotted border-green-500 p-5">
+          <FileInput type="file" accept="image/*" />
+          <Button
+            type="button"
+            gradientDuoTone="purpleToPink"
+            size="sm"
+            outline
+          >
+            Upload image
+          </Button>
+        </div>
+        <ReactQuill
+          theme="snow"
+          placeholder="Write something..."
+          className="h-72 mb-12"
+          required
+        />
+
+        <Button
+          gradientDuoTone="purpleToPink"
+          outline
+          type="submit"
+          className="w-full mt-5"
+        >
+          Publish
+        </Button>
+      </form>
+    </div>
+  );
+}
