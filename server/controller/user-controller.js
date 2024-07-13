9c9b21ad2a351d3ab1bf2d467bbd784e35e5db0a
Priyansh <priyanshpatel234@gmail.com>
2024-07-13 14:05:51 +0530

    create get users api route

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -56,6 +56,10 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+// ***********************************
+// Delete User data
+// ***********************************
+
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to Delete user"));
@@ -68,12 +72,62 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+// ***********************************
+// Signout Logic
+// ***********************************
+
 export const signout = async (req, res, next) => {
   try {
     res
       .clearCookie("access_token")
       .status(200)
       .json("User has been signed out");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+// Get All User data
+// ***********************************
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sortDirection === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: userWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
   } catch (error) {
     next(error);
   }
